5aec000ab89b9b3ec99232ac9ab9887b941e9a64
SpectCOW <126259962+SpcFORK@users.noreply.github.com>
2024-04-03 14:36:41 +0000

    We add some bundle magic;

---
+++ src/encode/index.js
@@ -0,0 +1,5 @@
+const bullpress = require("./bullpress");
+
+module.exports = {
+  bullpress,
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+const database = require("./database/index"),
+  encode = require("./encode/index"),
+  encrypt = require("./encrypt/index"),
+  user = require("./user/index");
+
+module.exports = {
+  database,
+  encode,
+  encrypt,
+  user,
+};
